2a4190e9e5d0203f9af9459bb1d0a2656c0d438f
Eduardo Perez <edujpp1@gmail.com>
2019-09-24 16:38:32 -0300

    Se prueba el correcto funcionamiento del método obtenerPuntuación

--- js/test.js
+++ js/test.js
@@ -1,10 +1,16 @@
-const expect = chai.expect;
+var expect = chai.expect;
+
 
 // Restaurant para realizar pruebas sobre los metodos de esta clase
-var restaurant = 	new Restaurant(23, 'Chez Moi', 'Ensalada', 'París', ['11:00', '12:00', '14:30'], '../img/ensalada1.jpg', [8, 4, 5, 5, 5, 5]);
+var restaurant = 	new Restaurant(0, '', '', '', [], '', []);
+
 
 // Probar método reservarHorario(horario)
-describe('Test de reservarHorario(horario)', function(){
+describe('Prueba de reservarHorario(horario)', function(){
+
+  // Se agregan valores al atributo horario
+  restaurant.horarios = ['11:00', '12:00', '14:30'];
+
   it('Se elimina del arreglo un horario cuando es reservado', function(){
     restaurant.reservarHorario('12:00');
     expect(restaurant.horarios).to.be.an('array').that.does.not.include('12:00');
@@ -17,4 +23,19 @@ describe('Test de reservarHorario(horario)', function(){
     restaurant.reservarHorario();
     expect(restaurant.horarios).to.deep.equal(['11:00', '14:30']);
   });
-});
+})
+
+
+// Probar método obtenerPuntuacion()
+describe('Prueba de obtenerPuntuacion()', function(){
+  it('Dado un restaurant sin calificaciones la misma es igual a 0', function (){
+    expect(restaurant.obtenerPuntuacion()).to.equal(0);
+  });
+  it('Dadas las calificaciones un restarant se calcula el promedio de las mismas de manera correcta', function(){
+    
+    // Se agregan valores al atributo calificaciones
+    restaurant.calificaciones = [9, 5, 7, 6, 7];
+    
+    expect(restaurant.obtenerPuntuacion()).to.equal(6.8);
+  });
+})
